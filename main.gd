3874c7c85051c23691578fccba87c5a4d41d12eb
niels1020 <134505218+niels1020@users.noreply.github.com>
2024-06-11 20:59:27 +0200

    bird setup

---
+++ main.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var poly = $poly
+
+@export var size:Vector2i
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var window = get_window()
+	if Input.is_action_just_pressed("escape"):
+		get_tree().quit()
+	
+	window.size = size
+	window.position = Vector2i(1000,0)
+	window.position = get_mouse_pos()
+
+func get_mouse_pos():
+	return Vector2(Vector2i(get_global_mouse_position())+get_window().position)
