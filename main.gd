10343eb8492c5b960b396dc4817069723b4659e7
niels1020 <134505218+niels1020@users.noreply.github.com>
2024-06-12 16:19:02 +0200

    just saving progress

--- main.gd
+++ main.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 @onready var poly = $poly
-
 @export var size:Vector2i
 
 # Called when the node enters the scene tree for the first time.
@@ -14,10 +13,10 @@ func _process(_delta):
 	var window = get_window()
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit()
-	
-	window.size = size
-	window.position = Vector2i(1000,0)
-	window.position = get_mouse_pos()
 
+	window.size = size
+	window.position = Vector2i(100,100)
+	var i:Image = DisplayServer.screen_get_image(0)
+	
 func get_mouse_pos():
 	return Vector2(Vector2i(get_global_mouse_position())+get_window().position)
